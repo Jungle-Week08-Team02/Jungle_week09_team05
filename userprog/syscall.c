c447e0455f9d431b68a7b99ba9db8b3d69a21dbc
ChoiMinKyu <chlalsrb118@gmail.com>
2024-11-19 04:55:32 +0000

    SystemCall_0.3

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -11,6 +11,7 @@
 /* Project 2 : System Call 구현 */
 #include <string.h>
 #include "filesys/file.h"
+#include "filesys/filesys.h"
 #include "threads/mmu.h"
 #include "threads/palloc.h"
 #include "userprog/process.h"
@@ -18,6 +19,9 @@
 
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
+
+/* Project 2. System Call 구현: filesys 위한 전역 lock */
+struct lock filesys_lock;
 
 /* System call.
  *
@@ -43,6 +47,9 @@ syscall_init (void) {
 	 * mode stack. Therefore, we masked the FLAG_FL. */
 	write_msr(MSR_SYSCALL_MASK,
 			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+	
+	/* Project 2. Syscall 구현 -> 전역 lock 여기서 초기화 */
+	lock_init(&filesys_lock);
 }
 
 /* The main system call interface */
@@ -74,15 +81,43 @@ syscall_handler (struct intr_frame *f UNUSED) {
 			break;
 		
 		case SYS_CREATE:
-			/* code */
+			f->R.rax = create(f->R.rdi, f->R.rsi);
 			break;
 		
 		case SYS_REMOVE:
-			/* code */
+			f->R.rax = remove(f->R.rdi);
 			break;
 		
 		case SYS_OPEN:
-			/* code */
+			f->R.rax = open(f->R.rdi);
+			break;
+
+		case SYS_FILESIZE:
+			f->R.rax = filesize(f->R.rdi);
+			break;
+	
+		case SYS_READ:
+			f->R.rax = read(f->R.rdi, f->R.rsi, f->R.rdx);
+			break;
+
+		case SYS_WRITE:
+			f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
+			break;
+		
+		case SYS_SEEK:
+				seek(f->R.rdi, f->R.rsi);
+				break;
+
+		case SYS_TELL:
+			f->R.rax = tell(f->R.rdi);
+			break;
+		
+		case SYS_CLOSE:
+			close(f->R.rdi);
+			break;
+		
+		case SYS_DUP2:
+			f->R.rax = dup2(f->R.rdi, f->R.rsi);
 			break;
 		
 		default:
@@ -140,4 +175,88 @@ int exec (const char *cmd_line){
 
 int wait (tid_t tid){
 	return process_wait(tid);
+}
+
+bool create(const char *file, unsigned initial_size){
+	check_address(file);
+
+	lock_acquire(&filesys_lock);
+	bool success = filesys_create(file, initial_size);
+	lock_release(&filesys_lock);
+
+	return success;
+}
+
+bool remove(const char *file){
+	check_address(file);
+
+	lock_acquire(&filesys_lock);
+	struct file *newfile = filesys_open(file);
+
+	if (newfile == NULL)
+		goto err;
+	
+	int fd = process_add_file(newfile);
+
+	if (fd == -1)
+		file_close(newfile);
+
+	lock_release(&filesys_lock);
+	return fd;
+err:
+	lock_release(&filesys_lock);
+	return -1;
+}
+///* 현재 process에 file을 추가하거나 가져오는 함수들 *///
+// 1. 현재 스레드의 fdt에 파일 추가 (현재 fd 다음에 추가)
+int process_add_file(struct file *f){
+	struct thread *curr = thread_current();
+	struct file **fdt = curr->fdt;
+
+	if (curr->fd_idx >= FDCOUNT_LIMIT)
+		return -1;
+	
+	while (fdt[curr->fd_idx] != NULL)
+		curr->fd_idx++;
+	
+	fdt[curr->fd_idx++] = f;
+
+	return curr->fd_idx - 1;
+}
+
+// 2. 현재 스레드의 fdt의 fd인덱스에 해당하는 파일 get
+struct file* process_get_file(int fd){
+	struct thread *curr = thread_current();
+
+	if (fd < 0 || fd >= FDCOUNT_LIMIT)
+		return NULL;
+	
+	return curr->fdt[fd];
+}
+
+// 3. 현재 스레드의 fdt의 fd인덱스에 해당하는 파일 삭제
+int process_close_file(int fd){
+	struct thread *curr = thread_current();
+
+	if (fd < 0 || fd > FDCOUNT_LIMIT)
+		return -1;
+	
+	curr->fdt[fd] = NULL;
+	return 0;
+}
+
+// 4. 현재 스레드의 fdt의 fd인덱스에 파일 insert
+int process_insert_file(int fd, struct file *f){
+	struct thread *curr = thread_current();
+	struct file **fdt = curr->fdt;
+
+	if (fd < 0 || fd >= FDCOUNT_LIMIT)
+		return -1;
+	
+	if (f>STDERR)
+		f->dup_count++;
+
+	fdt[fd] = f;
+
+	return fd;
 }
